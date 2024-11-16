9b1ce36f5818837c015f97c0defd6b37b4f5b784
ankit38yg <ankitkkkt78@gmail.com>
2024-11-16 12:35:10 +0530

    connecting to mongodb(database)

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import "dotenv/config";
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongodb is connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 const app = express();
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
+//ankitkkt78
+//HsZDtJLWTMJwDg4c
+//mongodb+srv://ankitkkt78:<db_password>@ankit-s-blog.x01nb.mongodb.net/?retryWrites=true&w=majority&appName=Ankit-s-blog
