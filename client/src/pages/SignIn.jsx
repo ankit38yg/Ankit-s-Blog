0b330895ad956324828f797dccf60867601e1bdd
ankit38yg <ankitkkkt78@gmail.com>
2024-11-17 22:05:10 +0530

    completing signin page all fuctionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,110 @@
-import React from 'react'
+import { Button, Label, TextInput, Alert, Spinner } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navitgate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return setErrorMessage("All field are required");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signIn", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navitgate("/");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>SignIn</div>
-  )
+    <div className="min-h-screen mt-20 ">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-orange-600 via-red-400 to-orange-300 rounded-lg text-teal-900">
+              Ankit's
+            </span>{" "}
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            Welcome to my blog! I'm Ankit Yadav, a passionate Computer Science
+            student with a knack for exploring innovative tech solutions.
+          </p>
+          <br />
+         <p>use email and entre your password to signin</p>
+        </div>
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value="your-email" />
+              <TextInput
+                type="email"
+                placeholder="email@company.com"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="your-password" />
+              <TextInput
+                type="password"
+                placeholder="password..."
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              type="submit"
+              gradientDuoTone="pinkToOrange"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loadind...</span>
+                </>
+              ) : (
+                "SignIn"
+              )}
+            </Button>
+          </form>
+          <div className="mt-5 gap-2 flex text-sm">
+            <span>Dont't have an account ?</span>
+            <Link to="/signUp" className="text-blue-500">
+              signup
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
